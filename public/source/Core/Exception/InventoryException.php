c14585a4c09fae47afe4636de3affda4ea92468d
Sandor Semsey <semseysandor@gmail.com>
2020-03-17 13:33:23 +0100

    restructure

---
+++ public/source/Core/Exception/InventoryException.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * +---------------------------------------------------------------------+
+ * | This file is part of chem-inventory.                                |
+ * |                                                                     |
+ * | Copyright (c) 2020 Sandor Semsey                                    |
+ * | All rights reserved.                                                |
+ * |                                                                     |
+ * | This work is published under the MIT License.                       |
+ * | https://choosealicense.com/licenses/mit/                            |
+ * |                                                                     |
+ * | It's a free software;)                                              |
+ * |                                                                     |
+ * | THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,     |
+ * | EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES     |
+ * | OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND            |
+ * | NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS |
+ * | BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN  |
+ * | ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN   |
+ * | CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE    |
+ * | SOFTWARE.                                                           |
+ * +---------------------------------------------------------------------+
+ */
+
+namespace Inventory\Core\Exception;
+
+use Exception;
+
+/**
+ * Base class for Exceptions
+ *
+ * @category Exception
+ * @package  Inventory
+ * @author   Sandor Semsey <semseysandor@gmail.com>
+ * @license  MIT https://choosealicense.com/licenses/mit/
+ * php version 7.4
+ */
+class InventoryException extends Exception
+{
+    /**
+     * Context of the exception
+     *
+     * @var string
+     */
+    protected string $context;
+
+    /**
+     * InventoryException constructor.
+     *
+     * @param string|null $context Context of the exception
+     */
+    public function __construct(string $context = null)
+    {
+        $this->context = $context;
+        parent::__construct();
+    }
+
+    /**
+     * Gets context
+     *
+     * @return string|null
+     */
+    public function getContext(): string
+    {
+        return $this->context;
+    }
+
+    /**
+     * Prints debug information
+     *
+     * @return void
+     */
+    public function print(): void
+    {
+        // todo: Exception string
+        echo "\nException\n".$this->getMessage()." at: ".$this->getContext()."\n";
+    }
+}
