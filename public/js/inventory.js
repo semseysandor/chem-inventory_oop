d1836aba4167a79f6642998019f1e5a50e748669
Sandor Semsey <semseysandor@gmail.com>
2020-04-14 19:46:37 +0200

    basic js

---
+++ public/js/inventory.js
@@ -0,0 +1,18 @@
+/*
+ +-----------------------------------------------+
+ | This file is part of chem-inventory.          |
+ |                                               |
+ | Copyright 2020                                |
+ | Sandor Semsey <semseysandor@gmail.com>        |
+ | All rights reserved.                          |
+ |                                               |
+ | This work is published under the MIT License. |
+ | https://choosealicense.com/licenses/mit/      |
+ +-----------------------------------------------+
+ */
+
+let Inventory
+
+function debug (message) {
+    console.log(message)
+}
