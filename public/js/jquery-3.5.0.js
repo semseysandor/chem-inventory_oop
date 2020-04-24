1c9d81b19c3a6ae90a50bad2c8c3c9d369da132a
Sandor Semsey <semseysandor@gmail.com>
2020-04-24 22:44:27 +0200

    jQuery

--- public/js/jquery-3.5.0.js
+++ public/js/jquery-3.5.0.js
@@ -1,15 +1,3 @@
-/*
- +-----------------------------------------------+
- | This file is part of chem-inventory.          |
- |                                               |
- | Copyright 2020                                |
- | Sandor Semsey <semseysandor@gmail.com>        |
- | All rights reserved.                          |
- |                                               |
- | This work is published under the MIT License. |
- | https://choosealicense.com/licenses/mit/      |
- +-----------------------------------------------+
- */
 (function (global, factory) {
 
     'use strict';
